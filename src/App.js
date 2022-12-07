4cc5c6d497a62da7d68a776b32a2694a92db306e
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2022-12-06 22:01:02 -0800

    all components created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 //Import all components
 import Form from './components/Form';
 import GiphDisplay from './components/GiphDisplay';
+import Button from './components/Button';
 
 
 function App () {
@@ -10,8 +11,11 @@ function App () {
 // all components in App's returned JSX
   return (
     <div className="App">
-      <GiphDisplay /> 
+      <Button /> 
       <Form /> 
+      <GiphDisplay /> 
+      
+      
 
     </div>
   )
---
+++ src/components/Button.js
@@ -0,0 +1,6 @@
+
+const Button = (props) => {
+    return <h1> this is the button </h1>
+}
+
+export default Button
